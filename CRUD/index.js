ce04a84063a0b8d51ff1c882ab49b3c0ed567730
UtkarshRB19o2 <ubandurkarcet@gmail.com>
2023-05-30 23:44:36 +0530

    my first commit

---
+++ CRUD/index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const user = require('./models.js')
+
+const url = 'mongodb://localhost:27017'
+
+mongoose.connect(url, {useNewUrlParser:true})
+
+const con = mongoose.connection
+con.on('open', function(){
+    console.log("connected")
+})
+
+
+const app = express()
+
+app.use(express.json())
+app.listen(9000, function(){
+    console.log("running.....")
+})
+
+app.post('/', async(req,res)=>{
+    const newuser = new user({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        age:req.body.age
+    })
+    try{
+        const u = await newuser.save()
+        res.json(u)
+    }
+    catch{
+        console.log('post error')
+    }
+})
+
+app.put('/:id', async(req,res)=>{
+    try{
+    const oneuser = await user.findById(req.params.id)
+        oneuser.name=req.body.name,
+        oneuser.email=req.body.email,
+        oneuser.password=req.body.password,
+        oneuser.age=req.body.age
+    
+        const u = oneuser.save()
+        res.json(u)
+    }
+    catch{
+        console.log('put error')
+    }
+})
+
+app.get('/:id', async(req,res)=>{
+    try{
+    const oneuser = await user.findById(req.params.id)    
+        res.json(oneuser)
+    }
+    catch{
+        console.log('get by id error')
+    }
+})
+
+app.get('/', async(req,res)=>{
+    try{
+    const all = await user.find()    
+        res.json(all)
+    }
+    catch{
+        console.log('get all error')
+    }
+})
+
+app.delete('/:id', async(req,res)=>{
+    try{
+        const oneuser = await user.findById(req.params.id)
+        oneuser.deleteOne()
+        res.send("deleted")
+    }
+    catch{
+        console.log("deletion error")
+    }
+})
